97c52729a4a04431a27772444b580c408d01a353
tdammy <tdammy92@gmail.com>
2023-12-09 08:01:09 +0100

    feat:add getAllExam endpoint

--- api/Database/DB.js
+++ api/Database/DB.js
@@ -2,17 +2,17 @@ const mongoose = require("mongoose");
 // process.env.S3_BUCKET
 
 mongoose.connect(
-	`mongodb+srv://ihdm:${process.env.DATABASE_PASSWORD}@bilek.f0ej3.mongodb.net/ipcmDb?retryWrites=true&w=majority`,
-	{ useNewUrlParser: true, useUnifiedTopology: true }
+  `mongodb+srv://ihdm:${process.env.DATABASE_PASSWORD}@bilek.f0ej3.mongodb.net/ipcmDb?retryWrites=true&w=majority`,
+  {}
 );
 
 const db = mongoose.connection;
 
 db.on(
-	"error",
-	console.error.bind(console, "DB was unable to connect connection error")
+  "error",
+  console.error.bind(console, "DB was unable to connect connection error")
 );
 
 db.once("open", function () {
-	console.log("Database is connected successfully");
+  console.log("Database is connected successfully");
 });
--- api/controllers/exam-controller/examController.js
+++ api/controllers/exam-controller/examController.js
@@ -1,26 +1,79 @@
 const examModel = require("../../Database/model/examsModel");
-const cloudinary = require("../../utils/cloudinary");
 
 // get all exams
 async function getAllExams(req, res) {
-  console.log("gettng all exams");
+  console.log("exam got called");
   try {
+    const response = await examModel.find();
+    //filter out answers, send only question to front end
+    const exams = response?.map((exam) => ({
+      exam_uuid: exam._id,
+      name: exam.examName,
+      duration: exam.duration,
+      createdAt: exam.updatedAt,
+      updatedAt: exam.updatedAt,
+      questions: exam?.questions?.map((qty) => {
+        return {
+          question: qty.question,
+          options: qty.options,
+          question_id: qty._id,
+        };
+      }),
+    }));
+
+    console.log("List of all exams", JSON.stringify(exams, null, 3));
+
     return res.status(200).json({
-      data: "All exams",
+      exams,
     });
   } catch (error) {
     return res.status(500).json({
-      message: "Something went wrong",
+      message: error,
     });
   }
 }
 
 //upload exams
 async function uploadExams(req, res) {
-  console.log("uploading exams");
+  //   console.log("uploading exams");
+
+  //   const newExam = {
+  //     examName: "WASH QUESTION_103335",
+  //     duration: 50,
+  //     questions: [
+  //       {
+  //         question:
+  //           "What are the four most important human needs in an emergency?",
+  //         options: [
+  //           "Money, electricity, freedom and health",
+  //           "Water, sanitation, food and shelter",
+  //           "Air, sleep, water and nutrition",
+  //           "Transportation, communication, clothing and food",
+  //         ],
+  //         answer: "Water, sanitation, food and shelter",
+  //       },
+  //       {
+  //         question:
+  //           "What is a potential outcome of a lack of access to basic water and sanitation services?",
+  //         options: [
+  //           "Increase in food production",
+  //           "Water, sanitation, food and shelter",
+  //           "Increase in economic growth",
+  //           "Reduction in poverty",
+  //           "Children are more susceptible to illness and death from diseases",
+  //         ],
+  //         answer:
+  //           "Children are more susceptible to illness and death from diseases",
+  //       },
+  //     ],
+  //   };
+
   try {
+    // const exam = new examModel(newExam);
+
+    // const result = await exam.save();
     return res.status(201).json({
-      data: "Exam uploaded",
+      data: [],
     });
   } catch (error) {
     return res.status(500).json({
